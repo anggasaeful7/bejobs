9e296c86d3716b45fa0a6af0cc5750eb4b6d5df3
anggasaeful7 <anggasaeful3@gmail.com>
2024-01-12 11:07:49 +0700

    Cek

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("indodaya", "root", "", {
+// const db = new Sequelize("indodaya", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+const db = new Sequelize("job", "admin", "adminCloud07", {
   host: "localhost",
   dialect: "mysql",
 });
