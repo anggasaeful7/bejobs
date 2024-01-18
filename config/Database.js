bcc3fbfeebe1923b7c7e8e8f37a5d20bb6d64c2c
anggasaeful7 <anggasaeful3@gmail.com>
2024-01-18 21:19:07 +0700

    FIX test

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("indodaya", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-// const db = new Sequelize("job", "admin", "adminCloud07", {
+// const db = new Sequelize("indodaya", "root", "", {
 //   host: "localhost",
 //   dialect: "mysql",
 // });
+
+const db = new Sequelize("job", "admin", "adminCloud07", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export default db;
